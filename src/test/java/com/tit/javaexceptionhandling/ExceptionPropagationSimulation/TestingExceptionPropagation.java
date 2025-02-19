d2d6463e171bc8252d1e380d6d1864b13efab043
Karan3872 <karananuragi7879@gmail.com>
2025-02-19 13:17:37 +0530

    [Karan Anuragi]Add.Program08 of Exception Propagation In Methods in Exception Handling

---
+++ src/test/java/com/tit/javaexceptionhandling/ExceptionPropagationSimulation/TestingExceptionPropagation.java
@@ -0,0 +1,24 @@
+package com.tit.javaexceptionhandling.ExceptionPropagationSimulation;
+
+import com.tit.javaexceptionhandling.executionpropagationinmethods.ExceptionPropagation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// Creating a class TestingExceptionPropagation to test exception propagation
+class TestingExceptionPropagation
+{
+
+    // Test method for exception propagation
+    @Test
+    void testExceptionPropagation()
+    {
+        // Creating an object of ExceptionPropagation
+        ExceptionPropagation ep = new ExceptionPropagation();
+
+        // Asserting that ArithmeticException is thrown when method2 is called
+        assertThrows(ArithmeticException.class, () -> {
+            ep.method2();
+        });
+    }
+}
